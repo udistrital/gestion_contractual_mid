07877a2c5462827fa61295267134c1f49dc1dbfc
kdgarzon <karendanielagarzon11@gmail.com>
2026-09-17 13:59:33 -0500

    test: adaptar pruebas unitarias de cargue masivo para el entorno de nestjs v11

--- src/cargue-masivo/cargue-masivo.service.spec.ts
+++ src/cargue-masivo/cargue-masivo.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CargueMasivoService } from './cargue-masivo.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('CargueMasivoService', () => {
   let service: CargueMasivoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CargueMasivoService],
+      providers: [
+        CargueMasivoService,
+        { provide: ConfigService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CargueMasivoService>(CargueMasivoService);
